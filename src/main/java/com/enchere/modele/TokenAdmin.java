5ee012298bf4a5e653bd6c968694007c9a7d3156
Marius405 <mariusrabe94@gmail.com>
2023-01-26 10:53:17 +0300

    Update TokenAdmin.java

--- src/main/java/com/enchere/modele/TokenAdmin.java
+++ src/main/java/com/enchere/modele/TokenAdmin.java
@@ -13,8 +13,8 @@ import java.sql.Timestamp;
 @Entity(name = "tokenadmin")
 public class TokenAdmin {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "s_token_admin")
-    @SequenceGenerator(name = "s_token_admin", sequenceName = "s_token_admin",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tokenadmin_id_seq")
+    @SequenceGenerator(name = "tokenadmin_id_seq", sequenceName = "tokenadmin_id_seq",allocationSize = 1)
     private Long id;
     private String valeur;
     @ManyToOne

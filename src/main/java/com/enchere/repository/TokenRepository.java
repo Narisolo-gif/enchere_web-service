4fc2fa4c3ade95f9b3835ce354d458d6129c9791
Marius405 <mariusrabe94@gmail.com>
2023-01-26 11:26:05 +0300

    Update TokenRepository.java

--- src/main/java/com/enchere/repository/TokenRepository.java
+++ src/main/java/com/enchere/repository/TokenRepository.java
@@ -9,5 +9,6 @@ import org.springframework.stereotype.Repository;
 public interface TokenRepository extends JpaRepository <Token, Long> {
     @Query(value = "SELECT * FROM token WHERE idutilisateur=?1 ORDER BY id LIMIT 1", nativeQuery = true)
     public Token getByIdUtilisateur(long id);
-
+    @Query(value = "DELETE from tokenadmin", nativeQuery = true)
+    public void deleteAll();
 }
